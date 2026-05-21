b817e59d3b8ac840d2427b4b7f8aa933262f5b81
hpeteralmeida <masterff0684@gmail.com>
2026-05-21 09:16:42 -0300

    feat: add exercise 46

---
+++ python/curso_em_video/mundo_2/exercicios/46countdown.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que faça a contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+
+for i in range(10, 0, -1):
+    print(i)
+    sleep(1)
+
+print("Feliz Ano Novo!")
