23d63520fa572236064ee9b0eb1a439783d8dcaa
hpeteralmeida <masterff0684@gmail.com>
2026-06-20 09:35:46 -0300

    add exercicio 7

---
+++ C/strings/exercicio_007.c
@@ -0,0 +1,17 @@
+/* Exercício 2
+
+Leia uma cidade usando scanf e imprima:
+Você mora em Goiânia */
+
+#include <stdio.h>
+
+int main() {
+    char cidade[40];
+
+    printf("Escreva o nome da sua cidade: ");
+    scanf("%s", cidade);
+
+    printf("Você mora em %s", cidade);
+
+    return 0;
+}
