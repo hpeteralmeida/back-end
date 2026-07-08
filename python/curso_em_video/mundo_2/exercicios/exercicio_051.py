61731575480380d782e55a8fb53f67832bc3e3d4
hpeteralmeida <masterff0684@gmail.com>
2026-07-08 14:53:30 -0300

    feat: add exercicio 51

--- python/curso_em_video/mundo_2/exercicios/exercicio_051.py
+++ python/curso_em_video/mundo_2/exercicios/exercicio_051.py
@@ -1,1 +1,12 @@
-#
+# Desenvolva um programa que leia o 
+# primeiro termo e a razão de uma PA. 
+# No final, mostre os 10 primeiros 
+# termos dessa progressão.
+
+inicio = int(input("Digite o primeiro termo da PA: "))
+razao = int(input("Digite a razão da PA: "))
+
+for i in range(10):
+    termo = inicio + i * razao
+    print(termo, end=" -> ")
+print("FIM")
